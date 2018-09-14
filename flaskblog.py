3e14b80a9b2b82efa7c8b593822a916b0754b179
flourishcodes <kunle@adeniran.com>
2018-09-14 08:29:18 -0700

    Uptade forms and register html template

--- flaskblog.py
+++ flaskblog.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfe280ba245'
 
 posts = [
     {'author': 'Kunle Adeniran',
@@ -27,10 +27,18 @@ def home():
 def about():
     return render_template("about.html", title='About')
 
+
 @app.route("/register")
 def register():
     form = RegistrationForm()
-    return render_template('register.html', title='Register', form=
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=forms)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
